f1e3d79af7064beb5d4da570e2169b73a761aa97
sureyes90 <susanreyes25@gmail.com>
2022-04-09 13:31:35 -0500

    prueba

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+const mainPage = document.querySelector('.main_home_page');
+const btnWelcome = document.querySelector('.btn_welcome');
+btnWelcome.addEventListener('click', showSecondPage);
+function showSecondPage(){
+    mainPage.innerHTML ="";
+    document.querySelector('.container_page_one').style.display = "visible";
+    document.querySelector('.container_page_two').style.display = "visible";
+}
+//return showSecondaryPage();
