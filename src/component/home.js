13fe203584da752988b317ea8e770849a509169f
sureyes90 <susanreyes25@gmail.com>
2022-04-11 17:56:41 -0500

    prueba 2 SPA

---
+++ src/component/home.js
@@ -0,0 +1,11 @@
+
+export const Home = () => {
+  const HomeDiv = document.createElement('div');
+  const buttonRegister = document.createElement('button');
+  const buttonLogin = document.createElement('button');
+
+  buttonRegister.textContent = 'Registrate';
+  buttonLogin.textContent = 'Inicia sesión';
+  
+    return HomeDiv;
+  };
