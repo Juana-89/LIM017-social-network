61d394b06dd5b38984e80e78d50ea699b970f2d8
Juana-89 <juana.lc@hotmail.es>
2022-05-18 15:43:56 -0500

    Updates

--- src/.firebase/index.js
+++
@@ -1,5 +0,0 @@
-export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js';
-export { getFirestore, setDoc, getDoc, addDoc, doc, collection, getDocs, onSnapshot,updateDoc, deleteDoc } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
-export { getStorage, ref, getDownloadURL, uploadBytesResumable } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-storage.js';
-export { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup, signInWithEmailAndPassword, 
-         createUserWithEmailAndPassword, sendPasswordResetEmail, onAuthStateChanged, signOut  } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js';
